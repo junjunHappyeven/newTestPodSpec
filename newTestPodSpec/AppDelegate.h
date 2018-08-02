41e79e0534356ba98a1f91ea91ff00ba83d5d504
湾流—俊俊的Mac <>
2018-08-02 10:33:32 +0800

    注释

---
+++ newTestPodSpec/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  newTestPodSpec
+//
+//  Created by 湾流—俊俊的Mac on 2018/8/2.
+//  Copyright © 2018年 湾流—俊俊的Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ newTestPodSpec/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  newTestPodSpec
+//
+//  Created by 湾流—俊俊的Mac on 2018/8/2.
+//  Copyright © 2018年 湾流—俊俊的Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
